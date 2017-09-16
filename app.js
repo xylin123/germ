d38c1a4ddb7dc87305194f310f3583d2ad14cf22
xylin123 <weishemebukeyi@vip.qq.com>
2017-09-16 14:07:39 +0800

    增加static

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const routes = require('./routes/routes');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.use(express.static(path.join(__dirname, 'static')));
+
 app.all('*', (req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Content-Type,Content-Length, Authorization, Accept,X-Requested-With');
